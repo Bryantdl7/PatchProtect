658da137b5e9b9b541ba6741410a969788271700
Bryant <bryantdl7@hotmail.com>
2025-03-03 14:55:47 -0500

    Update fonts.lua

--- lua/patchprotect/client/fonts.lua
+++ lua/patchprotect/client/fonts.lua
@@ -1,22 +1,23 @@
 local fonts = {}
+
 function cl_PProtect.setFont(f, s, b, a, sh, sy)
-  b, a, sh, sy = b or 500, a or false, sh or false, sy or false
-  local fstr = 'pprotect_' .. f .. '_' .. tostring(s) .. '_' .. tostring(b) .. '_' .. string.sub(tostring(a), 1, 1) .. '_' .. string.sub(tostring(sh), 1, 1)
+    b, a, sh, sy = b or 500, a or false, sh or false, sy or false
+    local fstr = string.format('pprotect_%s_%d_%d_%s_%s', f, s, b, tostring(a):sub(1, 1), tostring(sh):sub(1, 1))
 
-  if table.HasValue(fonts, fstr) then
+    if fonts[fstr] then
+        return fstr
+    end
+
+    surface.CreateFont(fstr, {
+        font = f,
+        size = s,
+        weight = b,
+        antialias = a,
+        shadow = sh,
+        symbol = sy
+    })
+
+    fonts[fstr] = true
+
     return fstr
-  end
-
-  surface.CreateFont(fstr, {
-    font = f,
-    size = s,
-    weight = b,
-    antialias = a,
-    shadow = sh,
-    symbol = sy
-  })
-
-  table.insert(fonts, fstr)
-
-  return fstr
 end
