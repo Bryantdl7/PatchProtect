9fec3e3c271452f2897b6be6e24a82342c0d46fe
Bryant <bryantdl7@hotmail.com>
2025-03-03 14:44:44 -0500

    Update config.lua

--- lua/patchprotect/server/config.lua
+++ lua/patchprotect/server/config.lua
@@ -1,77 +1,69 @@
------------------------
 --  NETWORK STRINGS  --
------------------------
+----------------------
 
 -- SETTINGS
-util.AddNetworkString('pprotect_new_settings')
-util.AddNetworkString('pprotect_save')
+local networkStrings = {
+    'pprotect_new_settings',
+    'pprotect_save',
+    'pprotect_cleanup',
+    'pprotect_new_counts',
+    'pprotect_buddy',
+    'pprotect_info_buddy',
+    'pprotect_send_buddies',
+    'pprotect_request_ents',
+    'pprotect_send_ents',
+    'pprotect_save_ents',
+    'pprotect_save_cent',
+    'pprotect_request_tools',
+    'pprotect_send_tools',
+    'pprotect_save_tools',
+    'pprotect_notify'
+}
 
--- CLEANUP
-util.AddNetworkString('pprotect_cleanup')
-util.AddNetworkString('pprotect_new_counts')
-
--- BUDDY
-util.AddNetworkString('pprotect_buddy')
-util.AddNetworkString('pprotect_info_buddy')
-util.AddNetworkString('pprotect_send_buddies')
-
--- BLOCKED PROPS/ENTS
-util.AddNetworkString('pprotect_request_ents')
-util.AddNetworkString('pprotect_send_ents')
-util.AddNetworkString('pprotect_save_ents')
-util.AddNetworkString('pprotect_save_cent')
-
--- ANTISPAMED/BLOCKED TOOLS
-util.AddNetworkString('pprotect_request_tools')
-util.AddNetworkString('pprotect_send_tools')
-util.AddNetworkString('pprotect_save_tools')
-
--- NOTIFICATIONS
-util.AddNetworkString('pprotect_notify')
+for _, v in ipairs(networkStrings) do
+    util.AddNetworkString(v)
+end
 
 ----------------------
 --  DEFAULT CONFIG  --
 ----------------------
 
-sv_PProtect.Config = {}
-
--- ANTI SPAM
-sv_PProtect.Config.Antispam = {
-  enabled = true,
-  admins = false,
-  alert = true,
-  prop = true,
-  tool = true,
-  toolblock = true,
-  propblock = true,
-  entblock = true,
-  propinprop = true,
-  cooldown = 0.3,
-  spam = 2,
-  spamaction = 'Nothing',
-  bantime = 10,
-  concommand = 'Put your command here'
-}
-
--- PROP PROTECTION
-sv_PProtect.Config.Propprotection = {
-  enabled = true,
-  superadmins = true,
-  admins = false,
-  adminscleanup = false,
-  use = true,
-  reload = true,
-  damage = true,
-  damageinvehicle = true,
-  gravgun = true,
-  proppickup = true,
-  creator = false,
-  propdriving = false,
-  worldpick = false,
-  worlduse = true,
-  worldtool = false,
-  worldgrav = true,
-  propdelete = true,
-  adminprops = false,
-  delay = 120
+sv_PProtect.Config = {
+    Antispam = {
+        enabled = true,
+        admins = false,
+        alert = true,
+        prop = true,
+        tool = true,
+        toolblock = true,
+        propblock = true,
+        entblock = true,
+        propinprop = true,
+        cooldown = 0.3,
+        spam = 2,
+        spamaction = 'Nothing',
+        bantime = 10,
+        concommand = 'Put your command here'
+    },
+    Propprotection = {
+        enabled = true,
+        superadmins = true,
+        admins = false,
+        adminscleanup = false,
+        use = true,
+        reload = true,
+        damage = true,
+        damageinvehicle = true,
+        gravgun = true,
+        proppickup = true,
+        creator = false,
+        propdriving = false,
+        worldpick = false,
+        worlduse = true,
+        worldtool = false,
+        worldgrav = true,
+        propdelete = true,
+        adminprops = false,
+        delay = 120
+    }
 }
