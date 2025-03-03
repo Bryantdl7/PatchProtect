d2415af9c21745a3a0269cbbbaacaee39a7fe294
Bryant <bryantdl7@hotmail.com>
2025-03-03 14:41:40 -0500

    optimization for the fun of it

--- lua/patchprotect/server/antispam.lua
+++ lua/patchprotect/server/antispam.lua
@@ -1,280 +1,194 @@
-----------------------
---  ANTISPAM SETUP  --
-----------------------
-
--- SET PLAYER VARS
+-- Setup Player Variables
 function sv_PProtect.Setup(ply)
-  -- Props
-  ply.propcooldown = 0
-  ply.props = 0
-
-  -- Tools
-  ply.toolcooldown = 0
-  ply.tools = 0
-
-  -- Duplicate
-  ply.duplicate = false
+    ply.propcooldown = 0
+    ply.props = 0
+    ply.toolcooldown = 0
+    ply.tools = 0
+    ply.duplicate = false
 end
 hook.Add('PlayerInitialSpawn', 'pprotect_initialspawn', sv_PProtect.Setup)
 
--- CHECK ANTISPAM ADMIN
+-- Check AntiSpam Admin Privileges
 function sv_PProtect.CheckASAdmin(ply)
-  if not IsValid(ply) then
-    return false
-  end
-  if !sv_PProtect.Settings.Antispam['enabled'] or ply:IsSuperAdmin() then
-    return true
-  end
-  if ply:IsAdmin() and sv_PProtect.Settings.Antispam['admins'] then
-    return true
-  end
-  return false
+    if not IsValid(ply) then return false end
+    return sv_PProtect.Settings.Antispam['enabled'] and not ply:IsSuperAdmin() and (not ply:IsAdmin() or not sv_PProtect.Settings.Antispam['admins'])
 end
 
--------------------
---  SPAM ACTION  --
--------------------
-
+-- Handle Spam Actions
 function sv_PProtect.spamaction(ply)
-  local action = sv_PProtect.Settings.Antispam['spamaction']
-  local name = ply:Nick()
+    local action = sv_PProtect.Settings.Antispam['spamaction']
+    local name = ply:Nick()
 
-  -- Cleanup
-  if action == 'Cleanup' then
-    cleanup.CC_Cleanup(ply, '', {})
-    sv_PProtect.Notify(ply, 'Cleaned all your props. (Reason: spamming)')
-    sv_PProtect.Notify(nil, 'Cleaned ' .. name .. 's props. (Reason: spamming)', 'admin')
-    print('[PatchProtect - AntiSpam] Cleaned ' .. name .. 's props. (Reason: spamming)')
-  -- Kick
-  elseif action == 'Kick' then
-    ply:Kick('Kicked by PatchProtect. (Reason: spamming)')
-    sv_PProtect.Notify(nil, 'Kicked ' .. name .. '. (Reason: spamming)', 'admin')
-    print('[PatchProtect - AntiSpam] Kicked ' .. name .. '. (Reason: spamming)')
-  -- Ban
-  elseif action == 'Ban' then
-    local mins = sv_PProtect.Settings.Antispam['bantime']
-    ply:Ban(mins, 'Banned by PatchProtect. (Reason: spamming)')
-    sv_PProtect.Notify(nil, 'Banned ' .. name .. ' for ' .. mins .. ' minutes. (Reason: spamming)', 'admin')
-    print('[PatchProtect - AntiSpam] Banned ' .. name .. ' for ' .. mins .. ' minutes. (Reason: spamming)')
-  -- ConCommand
-  elseif action == 'Command' then
-    if sv_PProtect.Settings.Antispam['concommand'] == sv_PProtect.Config.Antispam['concommand'] then return end
-    local rep = string.Replace(sv_PProtect.Settings.Antispam['concommand'], '<player>', ply:SteamID())
-    local cmd = string.Explode(' ', rep)
-    RunConsoleCommand(cmd[1], unpack(cmd, 2))
-    print("[PatchProtect - AntiSpam] Ran console command '" .. rep .. "'. (Reason: reached spam limit)")
-  end
+    if action == 'Cleanup' then
+        cleanup.CC_Cleanup(ply, '', {})
+        sv_PProtect.Notify(ply, 'Cleaned all your props. (Reason: spamming)')
+    elseif action == 'Kick' then
+        ply:Kick('Kicked by PatchProtect. (Reason: spamming)')
+    elseif action == 'Ban' then
+        local mins = sv_PProtect.Settings.Antispam['bantime']
+        ply:Ban(mins, 'Banned by PatchProtect. (Reason: spamming)')
+    elseif action == 'Command' then
+        local cmd = string.Replace(sv_PProtect.Settings.Antispam['concommand'], '<player>', ply:SteamID())
+        RunConsoleCommand(unpack(string.Explode(' ', cmd)))
+    end
+
+    local actionMessages = {
+        Cleanup = 'Cleaned all your props. (Reason: spamming)',
+        Kick = 'Kicked ' .. name .. '. (Reason: spamming)',
+        Ban = 'Banned ' .. name .. ' for ' .. mins .. ' minutes. (Reason: spamming)',
+        Command = "Ran console command '" .. cmd .. "'. (Reason: reached spam limit)"
+    }
+
+    sv_PProtect.Notify(nil, actionMessages[action], 'admin')
+    print('[PatchProtect - AntiSpam] ' .. actionMessages[action])
 end
 
------------------------
---  SPAWN ANTI SPAM  --
------------------------
-
+-- Handle AntiSpam for Prop/Entity Spawning
 function sv_PProtect.CanSpawn(ply, mdl)
-  if sv_PProtect.CheckASAdmin(ply) then return end
-  if ply.duplicate then return end
+    if sv_PProtect.CheckASAdmin(ply) or ply.duplicate then return end
+    mdl = string.lower(mdl)
+    
+    if string.find(mdl, '/../') or sv_PProtect.Blocked.props[mdl] or sv_PProtect.Blocked.ents[mdl] then
+        sv_PProtect.Notify(ply, 'This object is not allowed.')
+        return false
+    end
 
-  -- Block Prop/Entity with /../
-  if string.find(mdl, '/../') then return false end
+    if not sv_PProtect.Settings.Antispam['prop'] then return end
 
-  mdl = string.lower(mdl)
+    if CurTime() > ply.propcooldown then
+        ply.props = 0
+        ply.propcooldown = CurTime() + sv_PProtect.Settings.Antispam['cooldown']
+        return
+    end
 
-  -- Check if Prop/Entity is blocked
-  if (sv_PProtect.Settings.Antispam['propblock'] and sv_PProtect.Blocked.props[mdl]) or
-     (sv_PProtect.Settings.Antispam['entblock']  and sv_PProtect.Blocked.ents[mdl]) then
-    sv_PProtect.Notify(ply, 'This object is not allowed.')
+    ply.props = ply.props + 1
+    sv_PProtect.Notify(ply, 'Please wait ' .. math.Round(ply.propcooldown - CurTime(), 1) .. ' seconds', 'normal')
+
+    if ply.props >= sv_PProtect.Settings.Antispam['spam'] then
+        ply.props = 0
+        sv_PProtect.spamaction(ply)
+    end
+
     return false
-  end
-
-  -- If prop AntiSpam is not enabled, continue.
-  if !sv_PProtect.Settings.Antispam['prop'] then
-    return
-  end
-
-  -- Cooldown
-  if CurTime() > ply.propcooldown then
-    ply.props = 0
-    ply.propcooldown = CurTime() + sv_PProtect.Settings.Antispam['cooldown']
-    return
-  end
-
-  ply.props = ply.props + 1
-  sv_PProtect.Notify(ply, 'Please wait ' .. math.Round(ply.propcooldown - CurTime(), 1) .. ' seconds', 'normal')
-
-  -- Spamaction
-  if ply.props >= sv_PProtect.Settings.Antispam['spam'] then
-    ply.props = 0
-    sv_PProtect.spamaction(ply)
-    sv_PProtect.Notify(nil, ply:Nick() .. ' is spamming.', 'admin')
-    print('[PatchProtect - AntiSpam] ' .. ply:Nick() .. ' is spamming.')
-  end
-
-  return false
 end
-hook.Add('PlayerSpawnProp', 'pprotect_spawnprop', sv_PProtect.CanSpawn)
-hook.Add('PlayerSpawnEffect', 'pprotect_spawneffect', sv_PProtect.CanSpawn)
-hook.Add('PlayerSpawnSENT', 'pprotect_spawnSENT', sv_PProtect.CanSpawn)
-hook.Add('PlayerSpawnRagdoll', 'pprotect_spawnragdoll', sv_PProtect.CanSpawn)
-hook.Add('PlayerSpawnVehicle', 'pprotect_spawnvehicle', sv_PProtect.CanSpawn)
-hook.Add('PlayerSpawnNPC', 'pprotect_spawnNPC', sv_PProtect.CanSpawn)
-hook.Add('PlayerSpawnSWEP', 'pprotect_spawnSWEP', sv_PProtect.CanSpawn)
 
-----------------------
---  TOOL ANTI SPAM  --
-----------------------
+local spawnHooks = {
+    'PlayerSpawnProp', 'PlayerSpawnEffect', 'PlayerSpawnSENT',
+    'PlayerSpawnRagdoll', 'PlayerSpawnVehicle', 'PlayerSpawnNPC',
+    'PlayerSpawnSWEP'
+}
+for _, hookName in ipairs(spawnHooks) do
+    hook.Add(hookName, 'pprotect_' .. hookName, sv_PProtect.CanSpawn)
+end
 
--- TOOL-ANTISPAM
+-- Handle AntiSpam for Tool Usage
 function sv_PProtect.CanUseTool(ply, trace, tool)
-  if sv_PProtect.CheckASAdmin(ply) then return end
+    if sv_PProtect.CheckASAdmin(ply) then return end
 
-  -- Blocked Tool
-  if sv_PProtect.Settings.Antispam['toolblock'] and sv_PProtect.Blocked.btools[tool] then
-    sv_PProtect.Notify(ply, 'This tool is in the blacklist.', 'normal')
+    if sv_PProtect.Blocked.btools[tool] then
+        sv_PProtect.Notify(ply, 'This tool is in the blacklist.', 'normal')
+        return false
+    end
+
+    ply.duplicate = tool:find('duplicator') or tool:find('adv_duplicator')
+
+    if not sv_PProtect.Settings.Antispam['tool'] or not sv_PProtect.Blocked.atools[tool] then return end
+
+    if CurTime() > ply.toolcooldown then
+        ply.tools = 0
+        ply.toolcooldown = CurTime() + sv_PProtect.Settings.Antispam['cooldown']
+        return
+    end
+
+    ply.tools = ply.tools + 1
+    sv_PProtect.Notify(ply, 'Please wait ' .. math.Round(ply.toolcooldown - CurTime(), 1) .. ' seconds', 'normal')
+
+    if ply.tools >= sv_PProtect.Settings.Antispam['spam'] then
+        ply.tools = 0
+        sv_PProtect.spamaction(ply)
+    end
+
     return false
-  end
-
-  -- Check Dupe
-  if tool == 'duplicator' or tool == 'adv_duplicator' or tool == 'advdupe2' or tool == 'wire_adv' then
-    ply.duplicate = true
-  else
-    ply.duplicate = false
-  end
-
-  -- If tool AntiSpam is not enabled or the tool is not AntiSpam protected, continue.
-  if !sv_PProtect.Settings.Antispam['tool'] or !sv_PProtect.Blocked.atools[tool] then
-    return
-  end
-
-  -- Cooldown
-  if CurTime() > ply.toolcooldown then
-    ply.tools = 0
-    ply.toolcooldown = CurTime() + sv_PProtect.Settings.Antispam['cooldown']
-    return
-  end
-
-  ply.tools = ply.tools + 1
-  sv_PProtect.Notify(ply, 'Please wait ' .. math.Round(ply.toolcooldown - CurTime(), 1) .. ' seconds', 'normal')
-
-  -- Spamaction
-  if ply.tools >= sv_PProtect.Settings.Antispam['spam'] then
-    ply.tools = 0
-    sv_PProtect.spamaction(ply)
-    sv_PProtect.Notify(nil, ply:Nick() .. ' is spamming with ' .. tostring(tool) .. 's.', 'admin')
-    print('PatchProtect - AntiSpam] ' .. ply:Nick() .. ' is spamming with ' .. tostring(tool) .. 's.')
-  end
-
-  return false
 end
 hook.Add('CanTool', 'pprotect_tool_antispam', sv_PProtect.CanUseTool)
 
---------------------------
---  BLOCKED PROPS/ENTS  --
---------------------------
-
--- SEND BLOCKED PROPS/ENTS TABLE
-net.Receive('pprotect_request_ents', function(len, pl)
-  if !pl:IsSuperAdmin() then return end
-
-  local typ = net.ReadTable()[1]
-
-  net.Start('pprotect_send_ents')
-  net.WriteString(typ)
-  net.WriteTable(sv_PProtect.Blocked[typ])
-  net.Send(pl)
-end)
-
--- SAVE BLOCKED PROPS/ENTS TABLE
-net.Receive('pprotect_save_ents', function(len, pl)
-  if !pl:IsSuperAdmin() then return end
-
-  local d = net.ReadTable()
-  local typ, key = d[1], d[2]
-
-  sv_PProtect.Blocked[typ][key] = nil
-  sv_PProtect.saveBlockedEnts(typ, sv_PProtect.Blocked[typ])
-  print('[PatchProtect - AntiSpam] ' .. pl:Nick() .. ' removed ' .. key .. ' from the blocked-' .. typ .. '-list.')
-end)
-
--- SAVE BLOCKED PROP/ENT FROM CPANEL
-net.Receive('pprotect_save_cent', function(len, pl)
-  if !pl:IsSuperAdmin() then return end
-
-  local ent = net.ReadTable()
-
-  if sv_PProtect.Blocked[ent.typ][ent.name] then
-    sv_PProtect.Notify(pl, 'This object is already in the ' .. ent.typ .. '-list.', 'info')
-    return
-  end
-
-  sv_PProtect.Blocked[ent.typ][string.lower(ent.name)] = string.lower(ent.model)
-  sv_PProtect.saveBlockedEnts(ent.typ, sv_PProtect.Blocked[ent.typ])
-
-  sv_PProtect.Notify(pl, 'Saved ' .. ent.name .. ' to blocked-' .. ent.typ .. '-list.', 'info')
-  print('[PatchProtect - AntiSpam] ' .. pl:Nick() .. ' added ' .. ent.name .. ' to the blocked-' .. ent.typ .. '-list.')
-end)
-
--- IMPORT BLOCKED PROPS LIST
-concommand.Add('pprotect_import_blocked_props', function(ply, cmd, args)
-  if !pl:IsSuperAdmin() then return end
-
-  if !file.Read('pprotect_import_blocked_props.txt', 'DATA') then
-    print("Cannot find 'pprotect_import_blocked_props.txt' to import props. Please read the description of patchprotect.")
-    return
-  end
-
-  local imp = string.Explode(';', file.Read('pprotect_import_blocked_props.txt', 'DATA'))
-  table.foreach(imp, function(key, model)
-    if model == '' then return end
-    model = string.lower(string.sub(model, string.find(model, 'models/'), string.find(model, ';')))
-    if util.IsValidModel(model) and !sv_PProtect.Blocked.props[model] then
-      sv_PProtect.Blocked.props[model] = model
-    end
-  end)
-
-  sv_PProtect.saveBlockedEnts('props', sv_PProtect.Blocked.props)
-
-  print("\n[PatchProtect] Imported all blocked props. If you experience any errors,\nthen use the command to reset the whole blocked-props-list:\n'pprotect_reset blocked_props'\n")
-end)
-
---------------------------------
---  ANTISPAMED/BLOCKED TOOLS  --
---------------------------------
-
--- SEND ANTISPAMED/BLOCKED TOOLS TABLE
-net.Receive('pprotect_request_tools', function(len, pl)
-  if !pl:IsSuperAdmin() then return end
-
-  local t = string.sub(net.ReadTable()[1], 1, 1) .. 'tools'
-  local tools = {}
-
-  table.foreach(weapons.GetList(), function(_, wep)
-    if wep.ClassName != 'gmod_tool' then return end
-    table.foreach(wep.Tool, function(name, tool)
-      tools[name] = false
+-- Handle Blocked Props/Entities Communication
+local function handleBlockedEntities(netMsg, action)
+    net.Receive(netMsg, function(_, ply)
+        if not ply:IsSuperAdmin() then return end
+        local data = net.ReadTable()
+        action(ply, data)
     end)
-  end)
+end
 
-  table.foreach(sv_PProtect.Blocked[t], function(name, value)
-    if value == true then
-      tools[name] = true
+handleBlockedEntities('pprotect_request_ents', function(ply, typ)
+    net.Start('pprotect_send_ents')
+    net.WriteString(typ[1])
+    net.WriteTable(sv_PProtect.Blocked[typ[1]])
+    net.Send(ply)
+end)
+
+handleBlockedEntities('pprotect_save_ents', function(ply, data)
+    sv_PProtect.Blocked[data[1]][data[2]] = nil
+    sv_PProtect.saveBlockedEnts(data[1], sv_PProtect.Blocked[data[1]])
+    print('[PatchProtect - AntiSpam] ' .. ply:Nick() .. ' removed ' .. data[2] .. ' from the blocked-' .. data[1] .. '-list.')
+end)
+
+handleBlockedEntities('pprotect_save_cent', function(ply, ent)
+    local typ, name, model = ent.typ, ent.name, string.lower(ent.model)
+    if sv_PProtect.Blocked[typ][name] then
+        sv_PProtect.Notify(ply, 'This object is already in the ' .. typ .. '-list.', 'info')
+        return
     end
-  end)
-
-  net.Start('pprotect_send_tools')
-  net.WriteString(t)
-  net.WriteTable(tools)
-  net.Send(pl)
+    sv_PProtect.Blocked[typ][name] = model
+    sv_PProtect.saveBlockedEnts(typ, sv_PProtect.Blocked[typ])
+    sv_PProtect.Notify(ply, 'Saved ' .. name .. ' to blocked-' .. typ .. '-list.', 'info')
+    print('[PatchProtect - AntiSpam] ' .. ply:Nick() .. ' added ' .. name .. ' to the blocked-' .. typ .. '-list.')
 end)
 
--- SAVE BLOCKED/ANTISPAMED TOOLS
-net.Receive('pprotect_save_tools', function(len, pl)
-  if !pl:IsSuperAdmin() then return end
+-- Import Blocked Props List
+concommand.Add('pprotect_import_blocked_props', function(ply, cmd, args)
+    if not ply:IsSuperAdmin() then return end
 
-  local d = net.ReadTable()
-  local t1, t2, k, c = d[1], d[2], d[3], d[4]
+    local filePath = 'pprotect_import_blocked_props.txt'
+    if not file.Read(filePath, 'DATA') then
+        print("Cannot find 'pprotect_import_blocked_props.txt' to import props. Please read the description of PatchProtect.")
+        return
+    end
 
-  sv_PProtect.Blocked[t1][k] = c
-  sv_PProtect.saveBlockedTools(t2, sv_PProtect.Blocked[t1])
+    for _, model in ipairs(string.Explode(';', file.Read(filePath, 'DATA'))) do
+        if model ~= '' then
+            model = string.lower(string.sub(model, string.find(model, 'models/'), string.find(model, ';')))
+            if util.IsValidModel(model) and not sv_PProtect.Blocked.props[model] then
+                sv_PProtect.Blocked.props[model] = model
+            end
+        end
+    end
 
-  print('[PatchProtect - AntiSpam] ' .. pl:Nick() .. ' set "' .. k .. '" from ' .. t2 .. '-tools-list to "' .. tostring(c) .. '".')
+    sv_PProtect.saveBlockedEnts('props', sv_PProtect.Blocked.props)
+    print("\n[PatchProtect] Imported all blocked props. If you experience any errors,\nthen use the command to reset the whole blocked-props-list:\n'pprotect_reset blocked_props'\n")
 end)
+
+-- Handle AntiSpammed/Blocked Tools Communication
+handleBlockedEntities('pprotect_request_tools', function(ply, t)
+    local tools = {}
+    for _, wep in ipairs(weapons.GetList()) do
+        if wep.ClassName == 'gmod_tool' then
+            for name in pairs(wep.Tool) do
+                tools[name] = false
+            end
+        end
+    end
+    for name, value in pairs(sv_PProtect.Blocked[t[1] .. 'tools']) do
+        tools[name] = value
+    end
+    net.Start('pprotect_send_tools')
+    net.WriteString(t[1] .. 'tools')
+    net.WriteTable(tools)
+    net.Send(ply)
+end)
+
+handleBlockedEntities('pprotect_save_tools', function(ply, d)
+    sv_PProtect.Blocked[d[1]][d[3]] = d[4]
+    sv_PProtect.saveBlockedTools(d[2], sv_PProtect.Blocked[d[1]])
+    print('[PatchProtect - AntiSpam] ' .. ply:Nick() .. ' set "' .. d[3] .. '" from ' .. d[2] .. '-tools-list to "' .. tostring(d[4]) ..
